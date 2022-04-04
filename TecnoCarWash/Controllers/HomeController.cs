b9c550f380fd58d3102bbc76569cac366e6330f6
Cristian <cristianbr7@live.com>
2022-04-04 12:51:30 -0500

    first commit

---
+++ TecnoCarWash/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using TecnoCarWash.Models;
+
+namespace TecnoCarWash.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        var empleado = new Empleado()
+        {
+            Id = 1,
+            Edad = 52,
+            Nombre = "Choco Poderoso"
+        };
+        return View(empleado);
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
+
---
+++ TecnoCarWash/Models/Empleado.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace TecnoCarWash.Models
+{
+	public class Empleado
+	{
+        public long Id { get; set; }
+        public string Nombre { get; set; }
+        public int Edad { get; set; }
+    }
+}
+
